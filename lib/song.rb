b075353adb434da48902c72af909cd2aca26dacc
S. Makai Takori <makait87@gmail.com>
2020-01-30 17:28:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     song = file.split(" - ")[1]
     artist_name = file.split(" - ")[0]
     song_name = self.new(song)
-    song_name.artist_name = artist_name
+    song_name.artist_name = artist_name=
     song_name
   end
   
