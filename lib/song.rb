eb548e77c593445c30f059840442b9215648f4ba
S. Makai Takori <makait87@gmail.com>
2020-01-30 16:36:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,12 +13,6 @@ class Song
     @@all 
   end
   
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end  
-  
-  
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
     artist = file.split(" - ")[0]
@@ -27,5 +21,8 @@ class Song
     song
   end
   
-  
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end  
 end  
