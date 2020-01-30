5d3878e6caefc03d4228670359075848d97fe206
S. Makai Takori <makait87@gmail.com>
2020-01-30 17:07:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
   end
   
   def artist_name=(name)
-    @@all.artist_name = Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
     # self.artist.add_song(self)
   end  
 end  
